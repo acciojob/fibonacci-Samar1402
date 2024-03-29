747d7fdf87b64234b3cbceabea519de4290fa0b0
Samarjeet Kumar <124017057+Samar1402@users.noreply.github.com>
2024-03-29 11:26:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function fibonacci(num) {
 // your code here
-	let a=0,b=1,c,i=2;
-	console.log(a,b);
-	for(i=2;i<=num;i++){
+	let a=-1,b=1,c,i=2;
+	// console.log(a,b);
+	for(i=1;i<=num;i++){
 		c=a+b;
 		a=b;
 		b=c;
