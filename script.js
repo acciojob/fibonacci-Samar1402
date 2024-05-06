291c65aadbf3892435567f2e4f77ea476981bd59
Samarjeet Kumar <124017057+Samar1402@users.noreply.github.com>
2024-05-06 12:01:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 function fibonacci(num) {
 // your code here
-	let a=-1,b=1,c,i=2,s=" ";
+	let a=-1,b=1,c,i=2;
 	// console.log(a,b);
 	for(i=1;i<=num;i++){
 		c=a+b;
 		a=b;
 		b=c;
-		s+=c+","
 	}
-	return s;
+	return c;
 }
 // fibonacci(5);
 
